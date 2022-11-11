e21fd930b3d70135f64f7f667e1bae59fb532dfb
harpreet.moga <harpreet.singh@avidbots.com>
2022-11-11 14:15:36 -0500

    Updated

--- Serial_GUI/GUI_Master.py
+++ Serial_GUI/GUI_Master.py
@@ -19,10 +19,17 @@ class ComGui():
     def __init__(self, root):
         self.root = root
         self.frame = LabelFrame(root, text="Com Manager", padx=5, pady=5, bg ="white" )
+        self.label_com = Label(
+            self.frame, text="Availabel Port(s): ", bg = "white", width=15, anchor="w")
+        self.label_bd = Label(
+            self.frame, text="Baud Rate: ", bg = "white", width=15, anchor="w")
+        
+        self.publish()
+        
     def publish(self):
         self.frame.grid(row=0,column=0, rowspan=3, columnspan=3, padx=5,pady=5)
-        
-        
+        self.label_com.grid(column=1, row=2)
+        self.label_bd.grid(column=1, row=3)
         
         
 if __name__ == "__main__":
